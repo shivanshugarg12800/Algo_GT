85f5036a8682ffa407c83f981541255b2b24c257
Tohidujjaman Hoque <tohi7479@gmail.com>
2021-10-03 09:58:02 -0700

    Reverse a stack using Recursionn

---
+++ C++/Recursion/reverseStack.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void insertAtButtom(stack<int> &st, int ele){
+    if(st.empty()){
+        st.push(ele);
+        return;
+    }
+
+    int topele = st.top();
+    st.pop();
+    insertAtButtom(st,ele);
+
+    st.push(topele);
+}
+
+void reverse(stack<int> &st){
+    if(st.empty()){
+        return;
+    }
+
+    int ele= st.top();
+    st.pop();
+    reverse(st);
+    insertAtButtom(st,ele);
+}
+
+int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    reverse(st);
+    
+    while(!st.empty()){
+        cout << st.top() << endl;
+        st.pop();
+    }cout << endl;
+
+    return 0;
+}
